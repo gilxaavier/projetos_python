9dc2f8064445c343b10387f43f7ea6c26c41ee54
Gil Xavier <gilxaavier1@gmail.com>
2022-11-10 20:36:57 -0300

    2  atualização calculadora

--- main.py
+++ main.py
@@ -21,56 +21,45 @@ frame_corpo.grid(row=1, column=0)
 
 b_1 = Button(frame_corpo, text="C", width=11, height=2, bg=cor4, font=("Ivy 13 bold"), relief=RAISED, overrelief=RIDGE)
 b_1.place(x=0, y=0)
-
 b_2 = Button(frame_corpo, text="%", width=5, height=2, bg=cor4 , font=("Ivy 13 bold"), relief=RAISED, overrelief=RIDGE)
 b_2.place(x=118, y =0)
-
 b_3 = Button(frame_corpo, text="/", width=5, height=2, bg=cor5, fg=cor2, font=("Ivy 13 bold"), relief=RAISED, overrelief=RIDGE)
 b_3.place(x=177, y =0)
-
-
-b_4 = Button(frame_corpo, text="7", width=5, height=2, bg=cor4 , font=("Ivy 13 bold"), relief=RAISED, overrelief=RIDGE)
-b_4.place(x=0, y =52)
-b_4 = Button(frame_corpo, text="8", width=5, height=2, bg=cor4, font=("Ivy 13 bold"), relief=RAISED, overrelief=RIDGE)
-b_4.place(x=60, y =52)
-b_4 = Button(frame_corpo, text="9", width=5, height=2, bg=cor4 , font=("Ivy 13 bold"), relief=RAISED, overrelief=RIDGE)
-b_4.place(x=118, y =52)
-b_3 = Button(frame_corpo, text="*", width=5, height=2, bg=cor5, fg=cor2, font=("Ivy 13 bold"), relief=RAISED, overrelief=RIDGE)
-b_3.place(x=177, y =52)
-
-
 b_4 = Button(frame_corpo, text="7", width=5, height=2, bg=cor4 , font=("Ivy 13 bold"), relief=RAISED, overrelief=RIDGE)
 b_4.place(x=0, y =52)
 b_5 = Button(frame_corpo, text="8", width=5, height=2, bg=cor4, font=("Ivy 13 bold"), relief=RAISED, overrelief=RIDGE)
 b_5.place(x=60, y =52)
+
 b_6 = Button(frame_corpo, text="9", width=5, height=2, bg=cor4 , font=("Ivy 13 bold"), relief=RAISED, overrelief=RIDGE)
 b_6.place(x=118, y =52)
 b_7 = Button(frame_corpo, text="*", width=5, height=2, bg=cor5, fg=cor2, font=("Ivy 13 bold"), relief=RAISED, overrelief=RIDGE)
-b_7.place(x=177, y =52) 
-
-b_8 = Button(frame_corpo, text="7", width=5, height=2, bg=cor4 , font=("Ivy 13 bold"), relief=RAISED, overrelief=RIDGE)
-b_8.place(x=0, y =52)
-b_9 = Button(frame_corpo, text="8", width=5, height=2, bg=cor4, font=("Ivy 13 bold"), relief=RAISED, overrelief=RIDGE)
-b_9.place(x=60, y =52)
-b_10 = Button(frame_corpo, text="9", width=5, height=2, bg=cor4 , font=("Ivy 13 bold"), relief=RAISED, overrelief=RIDGE)
-b_10.place(x=118, y =52)
-b_11 = Button(frame_corpo, text="*", width=5, height=2, bg=cor5, fg=cor2, font=("Ivy 13 bold"), relief=RAISED, overrelief=RIDGE)
-b_11.place(x=177, y =52)
-
-b_12 = Button(frame_corpo, text="7", width=5, height=2, bg=cor4 , font=("Ivy 13 bold"), relief=RAISED, overrelief=RIDGE)
-b_12.place(x=0, y =52)
-b_13 = Button(frame_corpo, text="8", width=5, height=2, bg=cor4, font=("Ivy 13 bold"), relief=RAISED, overrelief=RIDGE)
-b_13.place(x=60, y =52)
-b_14 = Button(frame_corpo, text="9", width=5, height=2, bg=cor4 , font=("Ivy 13 bold"), relief=RAISED, overrelief=RIDGE)
-b_14.place(x=118, y =52)
-b_15 = Button(frame_corpo, text="*", width=5, height=2, bg=cor5, fg=cor2, font=("Ivy 13 bold"), relief=RAISED, overrelief=RIDGE)
-b_15.place(x=177, y =52)
+b_7.place(x=177, y =52)
 
 
+b_8 = Button(frame_corpo, text="4", width=5, height=2, bg=cor4 , font=("Ivy 13 bold"), relief=RAISED, overrelief=RIDGE)
+b_8.place(x=0, y =104)
+b_9 = Button(frame_corpo, text="5", width=5, height=2, bg=cor4, font=("Ivy 13 bold"), relief=RAISED, overrelief=RIDGE)
+b_9.place(x=60, y =104)
+b_10 = Button(frame_corpo, text="6", width=5, height=2, bg=cor4 , font=("Ivy 13 bold"), relief=RAISED, overrelief=RIDGE)
+b_10.place(x=118, y =104)
+b_11 = Button(frame_corpo, text="-", width=5, height=2, bg=cor5, fg=cor2, font=("Ivy 13 bold"), relief=RAISED, overrelief=RIDGE)
+b_11.place(x=177, y =104) 
 
+b_12 = Button(frame_corpo, text="1", width=5, height=2, bg=cor4 , font=("Ivy 13 bold"), relief=RAISED, overrelief=RIDGE)
+b_12.place(x=0, y =156)
+b_13 = Button(frame_corpo, text="2", width=5, height=2, bg=cor4, font=("Ivy 13 bold"), relief=RAISED, overrelief=RIDGE)
+b_13.place(x=60, y =156)
+b_14 = Button(frame_corpo, text="3", width=5, height=2, bg=cor4 , font=("Ivy 13 bold"), relief=RAISED, overrelief=RIDGE)
+b_14.place(x=118, y =156)
+b_15 = Button(frame_corpo, text="+", width=5, height=2, bg=cor5, fg=cor2, font=("Ivy 13 bold"), relief=RAISED, overrelief=RIDGE)
+b_15.place(x=177, y =156)
 
-
-
+b_16 = Button(frame_corpo, text="0", width=5, height=2, bg=cor4 , font=("Ivy 13 bold"), relief=RAISED, overrelief=RIDGE)
+b_16.place(x=0, y =212)
+b_17 = Button(frame_corpo, text=".", width=5, height=2, bg=cor4, font=("Ivy 13 bold"), relief=RAISED, overrelief=RIDGE)
+b_17.place(x=60, y =212)
+b_18 = Button(frame_corpo, text="=", width=5, height=2, bg=cor4 , font=("Ivy 13 bold"), relief=RAISED, overrelief=RIDGE)
+b_18.place(x=118, y =212)
 
 
 janela.mainloop()
