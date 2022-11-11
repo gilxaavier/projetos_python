d160429b8a0c6a677717a9441d9811fdb1abafa9
Gil Xavier <gilxaavier1@gmail.com>
2022-11-11 17:17:38 -0300

    atualização tela calculadora

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ frame_tela.grid(row=0, column=0)
 frame_corpo = Frame(janela, width=235, height=268) 
 frame_corpo.grid(row=1, column=0)
 
-app_label = Label(frame_tela, text="123456789", width=16, height=2, padx=7, relief=FLAT, )
+app_label = Label(frame_tela, text="123456789", width=16, height=2, padx=7, relief=FLAT, anchor="e", justify=RIGHT, font=("Ivy 17"))
 app_label.place(x=0, y=0 )
 
 
