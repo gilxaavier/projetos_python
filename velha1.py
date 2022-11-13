c6076532e1c89ac52632fdffef5017688ac417f2
Gil Xavier <gilxaavier1@gmail.com>
2022-11-13 09:32:11 -0300

    verificação jogador x

--- velha1.py
+++ velha1.py
@@ -1,3 +1,4 @@
+import sys
 print ("=" *30)
 print("Bem vindo ao jogo da velha")
 print("O jogo começa com o jogador 'x' e depois com o jogador 'o'")
@@ -73,3 +74,93 @@ if posicao_o == 8:
 print(f"|{peca0}|{peca1}|{peca2} ")
 print(f"|{peca3}|{peca4}|{peca5} ")
 print(f"|{peca6}|{peca7}|{peca8} ")
+
+
+
+posicao_x = int(input("Digite a posição da peça x (0-8): \n"))
+
+if posicao_x == 0:
+    peca0= "x"
+if posicao_x == 1:
+    peca1 = "x"
+if posicao_x == 2:
+    peca2 = "x"
+if posicao_x == 3:
+    peca3 = "x"
+if posicao_x == 4:
+    peca4 = "x"
+if posicao_x == 5:
+    peca5 = "x"
+if posicao_x == 6:
+    peca6 = "x"
+if posicao_x == 7:
+    peca7 = "x"
+if posicao_x ==8:
+    peca8 = "x"
+
+print(f"|{peca0}|{peca1}|{peca2} ")
+print(f"|{peca3}|{peca4}|{peca5} ")
+print(f"|{peca6}|{peca7}|{peca8} ")
+
+
+posicao_o = int(input("Digite a posicao da peca o (0-8):\n"))
+
+if(posicao_o == 0):
+    peca0 = 'o'
+if(posicao_o == 1):
+    peca1 = 'o'
+if(posicao_o == 2):
+    peca2 = 'o'
+if(posicao_o == 3):
+    peca3 = 'o'
+if(posicao_o == 4):
+    peca4 = 'o'
+if(posicao_o == 5):
+    peca5 = 'o'
+if(posicao_o == 6):
+    peca6 = 'o'
+if(posicao_o == 7):
+    peca7 = 'o'
+if(posicao_o == 8):
+    peca8 = 'o'
+
+print(f"|{peca0}|{peca1}|{peca2}|")
+print(f"|{peca3}|{peca4}|{peca5}|")
+print(f"|{peca6}|{peca7}|{peca8}|")
+
+posicao_x = int(input("Digite a posicao da peca x (0-8):\n"))
+
+if(posicao_x == 0):
+    peca0 = 'x'
+if(posicao_x == 1):
+    peca1 = 'x'
+if(posicao_x == 2):
+    peca2 = 'x'
+if(posicao_x == 3):
+    peca3 = 'x'
+if(posicao_x == 4):
+    peca4 = 'x'
+if(posicao_x == 5):
+    peca5 = 'x'
+if(posicao_x == 6):
+    peca6 = 'x'
+if(posicao_x == 7):
+    peca7 = 'x'
+if(posicao_x == 8):
+    peca8 = 'x'
+
+print(f"|{peca0}|{peca1}|{peca2}|")
+print(f"|{peca3}|{peca4}|{peca5}|")
+print(f"|{peca6}|{peca7}|{peca8}|")
+
+if (
+    (peca0 == peca1 == peca2 == "x") or
+    (peca3 == peca4 == peca5 == "x") or
+    (peca6 == peca7 == peca8 == "x") or
+    (peca1 == peca4 == peca7 == "x") or
+    (peca2 == peca5 == peca8 == "x") or
+    (peca0 == peca4 == peca8 == "x") or
+    (peca2 == peca4 == peca6 == "x") 
+):
+    print("Parabéns, o jodaor ganhou.")
+    sys.exit(0)
